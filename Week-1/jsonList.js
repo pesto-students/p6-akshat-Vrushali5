22bcbc4c9dd7b0c526f55c5d09555f6d0d34de77
vrushali <vrushali.hippargi@prolifics.com>
2022-09-29 17:32:59 +0530

    Week-1 assignment: Ex 1.2

---
+++ Week-1/jsonList.js
@@ -0,0 +1,35 @@
+const songList = [
+    { name: "1.One Dance", singer: "Crake feat CatKid", time: "2:54" },
+    { name: "2.Panda", singer: "Cattee", time: "4:06" },
+    {
+      name: `3.Can't Stop the Feeling!`,
+      singer: "Catin Cimberlake",
+      time: "3:56",
+    },
+    { name: "4.Work From Work", singer: "Cat Harmony feat Colla", time: "3:34" },
+  ];
+  
+  const playActionClassList = [
+    { name: `las la-step-backward` },
+    { name: `las la-pause` },
+    { name: `las la-step-forward` },
+    { name: `las la-undo-alt` },
+    { name: `las la-random` },
+  ];
+  
+  const navIconList = [
+    { name: `las la-bars` },
+    { name: `las la-home` },
+    { name: `las la-search` },
+    { name: `las la-volume-up` },
+    { name: `las la-user` },
+    { name: `las la-cog` },
+    { name: `lab la-spotify` },
+    { name: `lab la-soundcloud` },
+  ];
+  
+  const moreActionIcionList = [
+    { name: `las la-play`, title: "Play" },
+    { name: `las la-plus`, title: " Add to" },
+    { name: `las la-ellipsis-h`, title: "More" },
+  ];
